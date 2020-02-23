6571108c1829e1753be0da7d3fd523fff738d24b
Md. Al-Amin <alamin.fits@gmail.com>
2020-02-23 11:07:26 +0600

    Git discussion + OOP first class soure code added.

---
+++ Java Programming/EUIT_MAD_Class_007_OOP/src/com/company/Computer.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Computer {
+    public String name = "HP";
+    public String type = "Desktop";
+    public int processorCore = 3;
+
+
+    public void sayHello(){
+        System.out.println("Hello...");
+    }
+
+
+}
